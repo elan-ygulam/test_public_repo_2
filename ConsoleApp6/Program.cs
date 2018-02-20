4d46a7c29bdd0636a0d244c80e5f34ae95312104
elan-ygulam <34791752+elan-ygulam@users.noreply.github.com>
2018-02-20 11:46:51 +0000

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
